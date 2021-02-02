3b0ba39470bb6fa10072f162666ae86b9ab4b432
Mamadou Aly Sy <mamco.aly.sy@gmail.com>
2021-02-02 20:20:38 +0000

    adding history Model

---
+++ app/Models/History.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use LiteFramework\BaseClasses\Model;
+
+class History extends Model
+{
+    /**
+     * @var string
+     */
+    protected static string $tableName = 'history';
+}
