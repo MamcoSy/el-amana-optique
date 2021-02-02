2337b31bcaa3fc217689a23c55a294901c7a26fc
Mamadou Aly Sy <mamco.aly.sy@gmail.com>
2021-02-02 22:22:41 +0000

    database update

--- el-amana.sql
+++ el-amana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: database:3306
--- Generation Time: Jan 21, 2021 at 09:06 PM
+-- Generation Time: Feb 02, 2021 at 10:20 PM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.13
 
@@ -24,6 +24,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL,
+  `left_eye` varchar(195) NOT NULL,
+  `right_eye` varchar(195) NOT NULL,
+  `amount_to_pay` int(11) NOT NULL,
+  `paid_amount` int(11) NOT NULL,
+  `remaining_amount` int(11) NOT NULL,
+  `doctor_name` varchar(195) NOT NULL,
+  `mount_price` int(11) NOT NULL,
+  `left_eye_price` int(11) NOT NULL,
+  `right_eye_price` int(11) NOT NULL,
+  `client_name` varchar(195) NOT NULL,
+  `prescription_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`id`, `left_eye`, `right_eye`, `amount_to_pay`, `paid_amount`, `remaining_amount`, `doctor_name`, `mount_price`, `left_eye_price`, `right_eye_price`, `client_name`, `prescription_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, '3', '2.5', 12000, 2000, 10000, 'Cheikh Oumar Ndiaye', 2000, 5000, 5000, 'Zahra Sidi Addelaziz', NULL, 1, '2021-01-28', NULL),
+(2, '0.03', '-0.03', 30, 30, 0, 'test', 10, 10, 10, 'test', NULL, 1, '2021-02-01', NULL),
+(3, '2.8', '-2.5', 1500, 1500, 0, '', 500, 500, 500, 'Mamadou Aly Sy', NULL, 2, '2021-02-01', NULL),
+(5, '0.09', '-0.02', 1500, 1500, 0, 'Cheikh Oumar Ndiaye', 500, 500, 500, 'May sall', NULL, 2, '2021-02-02', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `prescriptions`
 --
 
@@ -34,7 +81,7 @@ CREATE TABLE `prescriptions` (
   `content` text NOT NULL,
   `left_eye` varchar(255) NOT NULL,
   `right_eye` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` date NOT NULL,
   `modified_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -43,7 +90,8 @@ CREATE TABLE `prescriptions` (
 --
 
 INSERT INTO `prescriptions` (`id`, `user_id`, `client_name`, `content`, `left_eye`, `right_eye`, `created_at`, `modified_at`) VALUES
-(1, 1, 'Zahra Sidi Abdoulaziz', 'Add + 1,50\r\nPhotoclique\r\nantirefle', '+3', '+2,5', '2021-01-21 19:36:22', NULL);
+(1, 1, 'Zahra Sidi Abdoulaziz', '<p>Add + 1,50</p><p>Photoclique</p><p>Antirefle\r\n</p>', '+3', '+2,5', '2021-01-21', NULL),
+(2, 1, 'May sall', '<p>antirefet</p>', '-9', '-8', '2021-01-28', NULL);
 
 -- --------------------------------------------------------
 
@@ -58,20 +106,34 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL DEFAULT '0',
-  `last_time_see` varchar(255) NOT NULL
+  `last_time_see` varchar(255) NOT NULL,
+  `created_at` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `role`, `last_time_see`) VALUES
-(1, 'Mamadou', 'Aly Sy', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 2, '21 / 01  / 2021 à 19:32:31'),
-(2, 'Samba', 'Ndiaye', 'samba', '00fd4b4549a1094aae926ef62e9dbd3cdcc2e456', 0, '21 / 01  / 2021 à 19:30:24');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `role`, `last_time_see`, `created_at`) VALUES
+(1, 'Mamadou', 'Aly Sy', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 2, '02 / 02  / 2021 à 20:04:09', '2021-01-01'),
+(2, 'Samba', 'Ndiaye', 'samba', '00fd4b4549a1094aae926ef62e9dbd3cdcc2e456', 0, '01 / 02  / 2021 à 13:18:33', '2021-02-01');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD KEY `fk_history_user_id` (`user_id`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_invoces_user_id` (`user_id`);
 
 --
 -- Indexes for table `prescriptions`
@@ -92,10 +154,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `prescriptions`
 --
 ALTER TABLE `prescriptions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -106,6 +174,18 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `history`
+--
+ALTER TABLE `history`
+  ADD CONSTRAINT `fk_history_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD CONSTRAINT `fk_invoces_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `prescriptions`
