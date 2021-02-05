1605b1aeace421ef90fbdb16789c5a5f4fd0110c
Th3-attacker <haje.malik5@gmail.com>
2021-02-05 21:39:36 +0000

    fixing url

--- views/partials/comptaBoard.blade.php
+++ views/partials/comptaBoard.blade.php
@@ -9,7 +9,7 @@
       <div class="icon">
         <i class="nav-icon fas fa-file-invoice-dollar"></i>
       </div>
-      <a href="#" class="small-box-footer">Plus d'infos <i class="fas fa-arrow-circle-right"></i></a>
+      <a href="{{url('/admin-panel/invoices')}}" class="small-box-footer">Plus d'infos <i class="fas fa-arrow-circle-right"></i></a>
     </div>
   </div>
   <div class="col-lg-6 col-6">
